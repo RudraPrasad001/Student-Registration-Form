e9e486cbf045a865dac04e945a032d5be9387e9b
RudraPrasad001 <rudraprasad.email@gmail.com>
2024-12-28 10:21:12 +0530

    React-DOM Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
+import Student  from "./Student"
+import ReactDOM from "react-dom/client";
 
 function App(){
   return(
+    
     <div className="root" id='root'>
+      
       <div className="Register">
       <h1>Student Register</h1>
       <span className = "details">Name:</span><input type="text" name="name" id="name" placeholder="Name"/><br /><br></br>
@@ -12,53 +15,37 @@ function App(){
       <input type="radio" name="isStudent"/>
       <span className = "details"> No</span>
       <input type="radio" name="isStudent"/><br/><br />
-      <button id="Submit" onClick={appendData}>Add</button></div>
+      <button id="Submit" onClick={()=>{appendDataToo()}}>Add</button></div>
+
 
     </div>
+    
 
   )
-  function appendData(){
+  function appendDataToo(){
     
     let textAge =document.getElementById("age")
     let textName = document.getElementById("name")
     let root = document.getElementById("root")
-    let divTag = document.createElement("div")
     let textIsStudent=document.getElementsByName("isStudent")
-   
-
+    
+    let divTag = document.createElement("div")
     divTag.className="studentData"
+    root.append(divTag)
+
+    const reactRoot = ReactDOM.createRoot(divTag)
+    if(textName.value==""){
+      textName.value="Guest"
+    }
+    reactRoot.render(
+      <Student name= {textName.value} age = {Number(textAge.value)} isStudent={textIsStudent[0].checked?"Yes":"No"}/>
+    ) 
    
-
-    let nameP =document.createElement("p")
-    nameP.append("Name: ")
-    if(textName.value===""){
-      nameP.append("Guest")
-    }
-    else{
-    nameP.append(textName.value)}
-
-    let ageP =document.createElement("p")
-    ageP.append("Age: ")
-    
-    if(isNaN(textAge.value) ||textAge.value===""){
-      ageP.append("Nil")
-   }
-    else{
-      ageP.append(textAge.value)
-    }
-
-    let isStudentP =document.createElement("p")
-    isStudentP.append("Is Student :")
-    isStudentP.append(textIsStudent[0].checked?"Yes":"No")
-    
-    divTag.appendChild(nameP)
-    divTag.appendChild(ageP)
-    divTag.appendChild(isStudentP)
-    root.appendChild(divTag)
     textAge.value="";
     textName.value="";
     textIsStudent[0].checked=false;
     textIsStudent[1].checked=false;
   }
+  
 }
 export default App
